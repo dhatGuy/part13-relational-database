0198779c5ad70d365f426f811f442e75773b6b9c
Joseph Odunsi <odunsiolakunbi@gmail.com>
2022-05-10 15:36:56 +0100

    13.5

--- index.js
+++ index.js
@@ -1,87 +1,20 @@
-require("dotenv").config();
-const { Sequelize, Model } = require("sequelize");
 const express = require("express");
 const app = express();
 
+const { PORT } = require("./util/config");
+const { connectToDatabase } = require("./util/db");
+
+const blogsRouter = require("./controllers/blogs");
+
 app.use(express.json());
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+app.use("/api/blogs", blogsRouter);
 
-class Blog extends Model {}
-
-Blog.init(
-  {
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    author: {
-      type: Sequelize.STRING,
-    },
-    url: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    title: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    likes: {
-      type: Sequelize.INTEGER,
-      defaultValue: 0,
-    },
-  },
-  {
-    sequelize,
-    modelName: "blogs",
-    underscored: true,
-    timestamps: false,
-  }
-);
-
-Blog.sync();
-
-app
-  .route("/api/blogs")
-  .get(async (req, res) => {
-    try {
-      const blogs = await Blog.findAll();
-      res.json(blogs);
-    } catch (error) {
-      return res.status(400).json({ error });
-    }
-  })
-  .post(async (req, res) => {
-    try {
-      const blog = await Blog.create(req.body);
-      res.json(blog);
-    } catch (error) {
-      return res.status(400).json({ error });
-    }
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
+};
 
-app.delete("/api/blogs/:id", async (req, res) => {
-  try {
-    const blog = await Blog.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-    res.json(blog);
-  } catch (error) {
-    return res.status(400).json({ error });
-  }
-});
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+start();
---
+++ models/blog.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require("sequelize");
+
+const { sequelize } = require("../util/db");
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.STRING,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    modelName: "blogs",
+    underscored: true,
+    timestamps: false,
+  }
+);
+
+module.exports = Blog;
