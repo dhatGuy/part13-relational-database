f1ba5d149590dd4dcf8b2e779b93282ca234018a
Joseph Odunsi <odunsiolakunbi@gmail.com>
2022-05-10 16:09:41 +0100

    13.6 - 13.7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,51 +2,57 @@ const router = require("express").Router();
 
 const { Blog } = require("../models");
 
-router.use(async (req, res, next) => {
+const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id);
   next();
-});
+};
 
-router.get("/", async (req, res) => {
-  const blogs = await Blog.findAll();
-  res.json(blogs);
-});
+router
+  .route("/")
+  .get(async (req, res) => {
+    const blogs = await Blog.findAll();
+    res.json(blogs);
+  })
+  .post(async (req, res) => {
+    try {
+      const blog = await Blog.create(req.body);
+      res.json(blog);
+    } catch (error) {
+      return res.status(400).json({ error });
+    }
+  });
 
-router.post("/", async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body);
-    res.json(blog);
-  } catch (error) {
-    return res.status(400).json({ error });
-  }
-});
-
-router.get("/:id", async (req, res) => {
-  const blog = req.blog;
-  if (blog) {
-    res.json(blog);
-  } else {
-    res.status(404).end();
-  }
-});
-
-router.delete("/:id", async (req, res) => {
-  const blog = req.blog;
-  if (blog) {
-    await blog.destroy();
-  }
-  res.status(204).end();
-});
-
-router.put("/:id", async (req, res) => {
-  const blog = req.blog;
-  if (blog) {
-    blog.important = req.body.important;
-    await blog.save();
-    res.json(blog);
-  } else {
-    res.status(404).end();
-  }
-});
+router
+  .use("/:id", blogFinder)
+  .route("/:id")
+  .get(async (req, res) => {
+    const blog = req.blog;
+    if (blog) {
+      res.json(blog);
+    } else {
+      res.status(404).end();
+    }
+  })
+  .delete(async (req, res) => {
+    const blog = req.blog;
+    if (blog) {
+      await blog.destroy();
+    }
+    res.status(204).end();
+  })
+  .put(async (req, res) => {
+    const blog = req.blog;
+    try {
+      if (blog) {
+        blog.likes = req.body.likes;
+        await blog.save();
+        res.json({ likes: blog.likes });
+      } else {
+        res.status(404).end();
+      }
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  });
 
 module.exports = router;
