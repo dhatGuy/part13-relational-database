ab5b2a6d4bab80cae9d62bfbde4e9e2f1350c5c7
Joseph Odunsi <odunsiolakunbi@gmail.com>
2022-05-12 04:26:17 +0100

    13.22

--- controllers/readingLists.js
+++ controllers/readingLists.js
@@ -4,6 +4,7 @@ const { Blog } = require("../models");
 const User = require("../models/user");
 const ReadingList = require("../models/reading_list");
 const { sequelize } = require("../util/db");
+const tokenExtractor = require("../middlewares/tokenExtractor");
 
 router.post("/", async (request, response) => {
   const { userId, blogId } = request.body;
@@ -14,6 +15,32 @@ router.post("/", async (request, response) => {
   });
 
   response.json(readingList);
+});
+
+router.put("/:id", tokenExtractor, async (request, response) => {
+  const { id } = request.params;
+  const { read } = request.body;
+
+  const readingList = await ReadingList.findOne({
+    where: {
+      id,
+    },
+  });
+
+  if (request.decodedToken.id == readingList.toJSON().userId) {
+    const update = await ReadingList.update(
+      { read },
+      {
+        where: {
+          id,
+        },
+      }
+    );
+
+    return response.json(update);
+  }
+
+  response.status(401).send("Unauthorized");
 });
 
 module.exports = router;
